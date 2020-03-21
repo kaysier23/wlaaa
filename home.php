15c2daa53bfdb44eddb8ffc22f141820004144fa
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-03-21 23:49:51 +0800

    Update home.php

--- home.php
+++ home.php
@@ -6,6 +6,7 @@
   margin-left: auto;
   margin-right: auto;
   width: 50%;">
+    <img src="<?php echo $_SESSION["user_image"] ?>" alt="profile pic" style="width:100%">
   <div style="text-align: center;
   padding: 10px 20px;">
   <h1>Profile Picture</h1>
