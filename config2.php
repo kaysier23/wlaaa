28ce71f3941c59f09a2719274f4695efe5e0f036
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-03-21 23:30:25 +0800

    Update config2.php

--- config2.php
+++ config2.php
@@ -5,8 +5,8 @@ require_once 'vendor/autoload.php';
 // Call Facebook API
 
 $facebook = new \Facebook\Facebook([
-  'app_id'      => '2621185531323878',
-  'app_secret'     => 'e96b253c6d17a45a674710eefe2153f1',
+  'app_id'      => '785266398672440',
+  'app_secret'     => 'd755827b4831a2f48db4013d83d52d4d',
   'default_graph_version'  => 'v2.10'
 ]);
 
